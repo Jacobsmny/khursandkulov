47fb7467db04e8bfbd89aad26193ea471c1e7aa5
Jacobsmny <khursandkulov@gmail.com>
2024-09-24 14:40:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const buyButton = document.getElementById('buy-button');
+    const purchaseForm = document.getElementById('purchase-form');
+
+    buyButton.addEventListener('click', () => {
+        purchaseForm.style.display = 'block';
+        purchaseForm.scrollIntoView({ behavior: 'smooth' });
+    });
+});
